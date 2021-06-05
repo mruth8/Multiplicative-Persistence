09921824707637a06ec8005a280de6eb1fc06bc1
Ruth, Matthew Raymond <mrruth2@illinois.edu>
2021-06-05 15:57:22 -0500

    Update multpers.c

--- multpers.c
+++ multpers.c
@@ -1,31 +1,41 @@
 #include "gmp.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-void fact(){
+int mp(char * numstr){
   int i;
   mpz_t p;
-  mpz_t cur;
-  char numstr[] = "6788";
-  int n = 4;
-  int k = 0;
-  mpz_set_str(cur, numstr, 10);
+  char digit;
+  int hasZero;
+  int pers = 0;
+  size_t n = strlen(numstr);
   do {
     mpz_init_set_ui(p,1); /* p = 1 */
+    hasZero = 0;
+    n = strlen(numstr);
     for (i=0; i < n ; ++i){
       mpz_mul_ui(p,p,(numstr[i]-'0')); /* p = p * num[i] */
     }
-    ++k;
-    printf ("%d - ", k);
+    ++pers;
+    printf ("%d - ", pers);
     mpz_out_str(stdout,10,p);
     printf ("\n");
-
-  } while (1 > 1);
+    mpz_get_str(numstr,10,p);
+  } while (mpz_sizeinbase(p,10) > 1);
   mpz_clear(p);
+  return pers;
 }
 
 int main(int argc, char * argv[]){
-  fact();
+  char str1[100];
+  str1[0] = '6';
+  str1[1] = '7';
+  str1[2] = '8';
+  str1[3] = '8';
+  str1[4] = '\0';
+  printf ("multiplicative persistence of %s is \n", str1);
+  mp(str1);
   return 1;
 }
